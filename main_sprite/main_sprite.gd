b3de85d3f31fb36726020a1da11ba105275c2afe
MarianMendoza <115280525+MarianMendoza@users.noreply.github.com>
2023-06-22 18:06:04 +0100

    Initial commit

---
+++ main_sprite/main_sprite.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+@export var move_speed : float = 100
+
+
+#Moving with keys.
+func _physics_process(_delta):
+	var input_direction = Vector2(
+		Input.get_action_strength("right") - Input.get_action_strength("left"),
+		Input.get_action_strength("down") - Input.get_action_strength("up")
+	)
+	
+	velocity = input_direction * move_speed
+	move_and_slide()
+	
