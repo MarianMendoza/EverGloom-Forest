5cd95f0a04c4b1e0abbb42139d6ae95d3b41980f
MarianMendoza <115280525+MarianMendoza@users.noreply.github.com>
2024-08-03 16:53:28 +0100

    Balloon edit config
    Add new balloon styles.

--- actionable.gd
+++ actionable.gd
@@ -1,7 +1,11 @@
 extends Area2D
 
+const Balloon = preload("res://dialouge/balloon.tscn")
 @export var dialogue_resource : DialogueResource
 @export var dialouge_start: String = "start"
 
-func action() -> void:
-	DialogueManager.show_example_dialogue_balloon(dialogue_resource, dialouge_start)
+
+func action () -> void:
+	var balloon: Node = Balloon.instantiate()
+	get_tree().current_scene.add_child(balloon)
+	balloon.start(dialogue_resource, dialouge_start)
